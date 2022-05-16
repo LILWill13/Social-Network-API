f40533ab5c4c3d27eab244e86af2f97ad53b4892
noah gaston <noahsfuture18@gmail.com>
2022-05-16 08:55:53 -0400

    users controller routes

---
+++ controllers/userControllers.js
@@ -0,0 +1,67 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, user } = require('../models');
+
+module.exports = { 
+    // get all users
+    getUsers(req, res) {
+        try{
+            User.find()
+            .then((users) =>  res.json(users))
+        } catch(err){
+            res.status(500).json(err);
+        };
+    },
+    // get single user by id
+    getAUser(req, res) {
+        try{
+            User.findOne({id:req.params.userId})
+            .select('-__v')
+            .then((user) => 
+            !user 
+                ? res.status(404).json({message: 'No user with this id'})
+                : res.json(user)
+        ); 
+        } catch(err){
+            res.status(500).json(err);
+        };
+    },
+    // create a new user
+    createUser(req, res) {
+        try{
+            User.create(req.body)
+            .then((user) => res.json(user))
+        } catch(err){
+            res.status(500).json(err);
+        };
+    },
+    // update users
+    updateAUser(req, res) {
+        try{
+            User.findOneAndUpdate(
+                {_id: req.params.userId },
+                {$set: req.body },
+                {runValidators: true, new: true }
+            )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({message: 'No user with this id'})
+                    : res.json(user)
+            );
+        } catch(err){
+            res.status(500).json(err);
+        };
+    },
+    // delete user
+    deleteUser(req, res) {
+        try{
+            User.findOneAndDelete({_id: req.params.userId })
+            .then((user) => 
+                !user
+                    ? res.status(404).json({ message: 'No user with this id' })
+                    : res.json({ message: 'User successfully deleted' })
+            );
+        } catch(err){
+            res.status(500).json(err);
+        };
+    },
+}
